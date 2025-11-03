c8ca6900cbc83a596391db2e173f2f70af16ca29
Taciano <taciolb@gmail.com>
2025-11-02 21:39:35 -0300

    commit inicial com esqueleto

---
+++ src/main/java/com/TLBTECH/proj_agendador_tarefas1/ProjAgendadorTarefas1Application.java
@@ -0,0 +1,13 @@
+package com.TLBTECH.proj_agendador_tarefas1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjAgendadorTarefas1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjAgendadorTarefas1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/TLBTECH/proj_agendador_tarefas1/ProjAgendadorTarefas1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TLBTECH.proj_agendador_tarefas1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjAgendadorTarefas1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
